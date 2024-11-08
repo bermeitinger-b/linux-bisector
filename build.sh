c9e8a0872a9f35993b891fbdbc5ada9cb36eaf93
Denis Benato <benato.denis96@gmail.com>
2024-11-08 20:58:26 +0100

    Fix directory

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ basedir=$(pwd)
 sudo rm -rf src pkg *.tar.zst *.tar.gz
 cd "$basedir/linux"
 git log -1 --pretty=format:"%H" | tee -a "$basedir/built_commits.txt"
-cd "$basedir/linux"
+cd "$basedir"
 tar --exclude-vcs -czvf linux.tar.gz ./linux/
 makepkg -sf
