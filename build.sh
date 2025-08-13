f488caeaada78485723795b372f293c70add759f
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2025-08-13 19:00:01 +0200

    less verbose tar and also delete *.sig
    Signed-off-by: Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 basedir="$(pwd)"
-rm -rf src pkg ./*.tar.zst ./*.tar.gz
+rm -rf src pkg ./*.tar.zst ./*.tar.zst.sig ./*.tar.gz
 cd "$basedir/linux" || exit
 git log -1 --pretty=format:"%H" | tee -a "$basedir/built_commits.txt"
 cd "$basedir" || exit
-tar --exclude-vcs -czvf linux.tar.gz ./linux/
+tar --exclude-vcs -czf linux.tar.gz ./linux/
 export BUILDDIR="${HOME}/.cache/makepkg"
 makepkg --syncdeps --force --sign
