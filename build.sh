f6ef5cf2fc3fdd75904dde90a6c7ca99fb06d2f1
Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>
2025-08-14 09:31:48 +0200

    fix built_commits creation
    Signed-off-by: Bernhard Bermeitinger <bernhard.bermeitinger@unisg.ch>

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 basedir="$(pwd)"
 rm -rf src pkg ./*.tar.zst ./*.tar.zst.sig ./*.tar.gz
 cd "$basedir/linux" || exit
-git log -1 --pretty=format:"%H" | tee -a "$basedir/built_commits.txt"
+git log -1 --pretty=format:"%H%n" | tee -a "$basedir/built_commits.txt"
 cd "$basedir" || exit
 tar --exclude-vcs -czf linux.tar.gz ./linux/
 export BUILDDIR="${HOME}/.cache/makepkg"
